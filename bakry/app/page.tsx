f527d999607a7d1a724d9ee24a16e63cde686100
Bakry Abdalsalam <67397650+bakryabdalsalam@users.noreply.github.com>
2024-06-08 19:17:41 +0000

    chore: Update .gitignore and package.json, add Clients component

--- bakry/app/page.tsx
+++ bakry/app/page.tsx
@@ -4,6 +4,11 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Grid from "@/components/Grid";
 import { navItems } from "@/data";
 import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -12,6 +17,10 @@ export default function Home() {
       <Hero />
       <Grid />
       <RecentProjects />
+      <Clients />
+      <Experience />
+      <Approach />
+      <Footer />
       </div>
     </main>
   );
